bd37f19ecac8aa85d3a3f45a6c34444df804452e
Emilio Moreno <emilio@touchof.tech>
2023-07-05 19:49:20 -0600

    feat: add endpoints, exceptions, and stuff

--- routes/inventory.py
+++ routes/inventory.py
@@ -2,10 +2,13 @@ from bson import ObjectId
 from typing import Annotated
 from fastapi import Depends, APIRouter
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
-from models.inventory import CreateInventory, GetInventory
+from models.inventory import CreateInventory
 from utils.database import get_db
+from utils.validate_token import validate_token
+from utils.exceptions import NotFoundRecord
 
 
 router = APIRouter(
@@ -14,19 +17,23 @@ router = APIRouter(
 )
 
 
-@router.get("", response_model=list[GetInventory])
+@router.get("", dependencies=[Depends(validate_token)])
 async def list_inventory(database: Annotated[AsyncIOMotorDatabase, Depends(get_db)]):
     inventory_list = [inventory async for inventory in database.inventory.find({})]
 
-    return jsonable_encoder(inventory_list)
+    return JSONResponse(
+        content=jsonable_encoder(inventory_list),
+        status_code=200
+    )
 
 
-@router.post("")
+
+@router.post("", dependencies=[Depends(validate_token)])
 async def create_inventory(
     create_inventory: CreateInventory,
     database: Annotated[AsyncIOMotorDatabase, Depends(get_db)],
 ):
-    await database.inventory.insert_one(
+    inserted_id = await database.inventory.insert_one(
         {
             "_id": str(ObjectId()),
             "name": create_inventory.name,
@@ -35,4 +42,27 @@ async def create_inventory(
         }
     )
 
-    return {"message": "ok"}
+    return JSONResponse(
+        content={"created_inventory": inserted_id.inserted_id},
+        status_code=201
+    )
+
+
+@router.get("/{inventory_id}")
+async def get_inventory(
+    database: Annotated[AsyncIOMotorDatabase, Depends(get_db)],
+    inventory_id: str,
+):
+    inventory = await database.inventory.find_one(
+        {
+            "_id": inventory_id,
+        }
+    )
+
+    if not inventory:
+        raise NotFoundRecord(f"Inventory with id {inventory_id} does not exists")
+
+    return JSONResponse(
+        content=inventory,
+        status_code=200
+    )
